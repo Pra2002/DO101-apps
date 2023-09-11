56eabb114b87bd337f928ca4fd99a0c9a40658eb
Prachiti <ptalgulk@redhat.com>
2023-09-11 06:00:00 -0400

    =new-planet

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -5,6 +5,9 @@ app.get('/', function (req, res) {
   res.send('Hello World -> Prachiti!\n');
 });
 
+app.get('/earth', function(req, res){
+  res.send("Earth");
+});
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
 });
